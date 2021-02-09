d923b35d1e443973a1a0bad559dd49243846de91
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-02-08 16:56:22 -0800

    testing mult test on ex.test.js

--- tests/example.test.js
+++ tests/example.test.js
@@ -8,4 +8,10 @@ test(`Greeting should be "Hello World!"`, () => {
     expect(greeting()).toBe('GoodBye World!')
 })
 
+test('Returned value should be typed string', () => {
+    expect(typeof greeting()).toBe('string')
+})
 
+test('Return string should not be empty', () => {
+    expect(greeting().length).toBeGreaterThan(0)
+})
