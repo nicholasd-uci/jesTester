9e9f579099a33ab793ccec8c76b690537e7c920f
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-02-08 16:32:18 -0800

    included ex.test.js - testing

---
+++ tests/example.test.js
@@ -0,0 +1,9 @@
+const { test, expect } = require("@jest/globals")
+
+const greeting = () => {
+    return "Hello World!"
+}
+
+test(`Greeting should be "Hello World!"`, () => {
+    expect(greeting()).toBe('Hello World!')
+})
